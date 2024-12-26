913c1b3aeb0733eb6705cd9b0fe66dac741f1c08
Vaclav Stebra <vstebra@gmail.com>
2024-12-26 21:45:27 +0100

    Solution for Day 2

---
+++ Solutions/2024/Day02.cs
@@ -0,0 +1,72 @@
+﻿namespace Solutions2024;
+
+public class Day02
+{
+    public int SolvePart01(string[] lines)
+    {
+        return ParseData(lines).Where(l => IsSafe(l)).Count();
+    }
+
+    public int SolvePart02(string[] lines)
+    {
+        return ParseData(lines).Where(x => IsSafe(x, true)).Count();
+    }
+
+    private IEnumerable<List<int>> ParseData(string[] lines)
+    {
+        return lines.Select(l => l.Split(" ").Select(int.Parse).ToList());
+    }
+
+    private bool IsSafe(List<int> parsed, bool tolerance = false)
+    {
+        int order = Ordering(parsed[0], parsed[1]);
+        for (int i = 0; i < parsed.Count - 1; i++)
+        {
+            int diff = Math.Abs(parsed[i] - parsed[i + 1]);
+            if (Ordering(parsed[i], parsed[i + 1]) != order)
+            {
+                if (!tolerance)
+                {
+                    return false;
+                }
+                else
+                {
+                    return IsSafeWithoutElement(parsed, i);
+                }
+            }
+            if (diff < 1 || diff > 3)
+            {
+                if (!tolerance)
+                {
+                    return false;
+                }
+                else
+                {
+                    return IsSafeWithoutElement(parsed, i);
+                }
+            }
+        }
+        return true;
+    }
+
+    private int Ordering(int first, int second)
+    {
+        if (first > second)
+        {
+            return 1;
+        }
+        else if (first == second)
+        {
+            return 0;
+        }
+        return -1;
+    }
+    private bool IsSafeWithoutElement(List<int> parsed, int i)
+    {
+        var x = parsed.Where((_, j) => j != i - 1).ToList();
+        var y = parsed.Where((_, j) => j != i).ToList();
+        var z = parsed.Where((_, j) => j != i + 1).ToList();
+        return IsSafe(x) || IsSafe(y) || IsSafe(z);
+    }
+
+}
---
+++ Tests/2024/Day02Test.cs
@@ -0,0 +1,24 @@
+﻿using Solutions2024;
+
+namespace Tests2024;
+
+public class Day02Test
+{
+    private readonly string[] EXAMPLE_INPUT = ["7 6 4 2 1", "1 2 7 8 9", "9 7 6 2 1", "1 3 2 4 5", "8 6 4 4 1", "1 3 6 7 9"];
+
+    [Fact]
+    public void Day02_part01_Example()
+    {
+        Day02 solver = new();
+        int result = solver.SolvePart01(EXAMPLE_INPUT);
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Day02_part02_Example()
+    {
+        Day02 solver = new();
+        int result = solver.SolvePart02(EXAMPLE_INPUT);
+        Assert.Equal(4, result);
+    }
+}
